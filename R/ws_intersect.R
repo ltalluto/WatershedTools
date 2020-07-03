f0907505e38b736d6a1ac34bb8e420a53c11c328
Matthew Talluto <mtalluto@gmail.com>
2020-07-03 20:05:31 +0200

    River buffer intersect

--- R/ws_intersect.R
+++ R/ws_intersect.R
@@ -7,29 +7,73 @@
 #' * `riparian`: The layers in `areas` are summarized within a riparian buffer surrounding each reach; 
 #'  		provided if `rip_buffer` is not NA.
 #' * `riparian_upstream`: The layers in `areas` are summarized within a riparian buffer for each focual reach and all
-#'          upstream reaches; provided if `rip_buffer` is not NA and `catchment` is TRUE
+#'          upstream reaches; provided if `rip_buffer` is not NA and `drainage` is not NA
 #' * `catchment`: The layers in `areas` are summarized within the entire catchment for each reach, provided if
-#'          `catchment` is TRUE
+#'          `drainage` is not NA
 #' 
 #' @param x A river network; this should be a vector gis feature, either from the `sf` or `sp` packages
 #' @param areas A named list of areal features to summarize; should be polygon features from `sf` or `sp`
 #' @param reach_id Name of the field in x that identifies stream segments
 #' @param rip_buffer The width of the riparian buffer; if NA, then no buffered analysis will be done, see 'details'
-#' @param catchment Boolean, if TRUE catchment area-based analyses will be done; see 'details'
+#' @param drainage Optional drainage direction raster for calculating catchment; see 'details'
 #' @param use_sf Boolean, if TRUE, functions from the sf package will be used whenever possible, if FALSE, GRASS will be used
 #' @param ... Additional parameters to pass to [GrassSession()]
-ws_intersect = function(x, areas, reach_id = "a_cat_", rip_buffer = 100, catchment = FALSE, use_sf = FALSE, ...) {
+#' @export
+ws_intersect = function(x, areas, reach_id = "a_cat_", rip_buffer = 100, drainage = NA, use_sf = FALSE, ...) {
 	if(is(x, "Spatial"))
 		x = sf::st_as_sf(x)
 	if(!is(x, "sf"))
 		stop("x must inherit from either sf or Spatial")
 	
-	# step 1: intersection
-	if(!is.na(rip_buffer)) {
-		if(use_sf) {
-			x_buff = sf::st_buffer(x, rip_buffer)
+	if(use_sf) {
+		gs = NULL
+	} else {
+		if(is.na(drainage)) {
+			ras = raster::raster(ext = raster::extent(x), crs = sp::proj4string(areas[[1]]), resolution  = 25)
+			layername = NA
 		} else {
-			gs = GrassSession(as(areas[[1]], "Spatial"), ...)
+			ras = drainage
+			layername = 'drainage'
 		}
+		gs = GrassSession(ras, layerName = layername, ...)
 	}
+	
+	# step 1: buffer and intersection
+	if(!is.na(rip_buffer)) {
+		rb_isect = mapply(.riv_buff_intersect, x = x, y = areas, moreArgs = list(rip_buffer = rip_buffer, gs = gs))
+	}
+
+	
+	# for(i in seq_along(areas))
+	# 	rgrass7::writeVECT(areas[[i]], vname = names(areas)[i])
 }
+
+#' Helper function for creating river buffers
+#' @param x A river network; this should be a vector gis feature, either from the `sf` or `sp` packages
+#' @param y The polygon feature to intersect, sf class
+#' @param rip_buffer The width of the riparian buffer
+#' @param gs A grass session, if missing or NA, then sf functions will be used
+#' @return If gs is null or grass_output is "sf", then a buffer polygon layer of class "sf", if grass_output is "string",
+#' then the name of the vector layer in the grass session
+#' @keywords internal
+.riv_buff_intersect = function(x, y, rip_buffer, gs) {
+	if(missing(gs) || is.na(gs)) {
+		x_buff = sf::st_buffer(x, rip_buffer)
+		x_isect = sf::st_intersection(x_buff, y)
+	} else {
+		rgrass7::writeVECT(as(x, "Spatial"), vname = "river", ignore.stderr=TRUE, v.in.ogr_flags="overwrite")
+		area_name = 'ri_polygon'
+		rgrass7::writeVECT(as(y, "Spatial"), vname = area_name, ignore.stderr=TRUE, v.in.ogr_flags="overwrite")
+		buff_name = 'riv_buff'
+		int_name = 'riv_isect'
+		rgrass7::execGRASS("v.buffer", flags = c('t', 'overwrite', 'quiet'), input = 'river', output = buff_name, 
+						   distance = rip_buffer, ignore.stderr=TRUE, Sys_ignore.stdout=TRUE)
+		rgrass7::execGRASS("v.overlay", flags = c('overwrite', 'quiet'), ainput = buff_name, binput = area_name, output = int_name, 
+						   operator='and', ignore.stderr=TRUE, Sys_ignore.stdout=TRUE)
+		x_isect = rgrass7::readVECT(int_name, ignore.stderr=TRUE)
+		if(is(x_isect, "Spatial"))
+			x_isect = sf::st_as_sf(x_isect)
+	}
+	return(x_isect)
+}
+
--- tests/testthat/test-ws_intersect.R
+++ tests/testthat/test-ws_intersect.R
@@ -1,3 +1,21 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(sf)
+riv = readRDS(system.file("testdata/ws_intersect/river.rds", package="WatershedTools"))
+litho = readRDS(system.file("testdata/ws_intersect/lith.rds", package="WatershedTools"))
+
+test_that("Buffering & Intersection works", {
+	skip_on_cran()
+	ras = raster::raster(ext = raster::extent(riv), crs = sf::st_crs(riv)$proj4string, resolution  = 25)
+	layername = NA
+	gs = GrassSession(ras, layerName = layername, gisBase = WatershedTools:::getGISBase())
+
+	expect_warning(isect_sf <- WatershedTools:::.riv_buff_intersect(riv, litho, 100, gs = NA), regex="attribute")
+	expect_error(isect_gr <- WatershedTools:::.riv_buff_intersect(riv, litho, 100, gs = gs), regex=NA)
+	
+	# these methods are really different, but at least expect similar areas for the intersection fields
+	# expect less than 1% error in area on average
+	s1 = reshape2::melt(tapply(st_area(isect_sf), list(isect_sf$xx, isect_sf$a_cat_), sum), na.rm=TRUE)
+	s2 = reshape2::melt(tapply(st_area(isect_gr), list(isect_gr$b_xx, isect_gr$a_a_cat_), sum), na.rm=TRUE)
+	s_merge = merge(s1, s2, by = c("Var1", "Var2"))
+	expect_lt(mean(abs(s_merge$value.x - s_merge$value.y) / s_merge$value.x), 0.01)
+	
 })
